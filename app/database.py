7180e6ddeb34cd65e0d0f915fecfe196690c83f1
Ajayrajc1998 <ajayrajc007@gmail.com>
2025-01-28 23:08:38 +0530

    refactor: improve database engine setup in database.py for clarity

--- app/database.py
+++ app/database.py
@@ -10,8 +10,13 @@ load_dotenv()
 try:
     DATABASE_URL = os.getenv("DATABASE_URL")
 
-    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+    # Create the SQLAlchemy engine
+    engine = create_engine(DATABASE_URL)
+
+    # Create a configured "Session" class
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    # Base class for our models
     Base = declarative_base()
 except Exception as e:
     print(f"database URL: {DATABASE_URL}")
